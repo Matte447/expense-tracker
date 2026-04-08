69e994105d6a3c5258f01c27375d8364abad2af7
Matteo Bienert <matteo.bienert@icloud.com>
2026-04-08 18:16:48 +0200

    Added income tracker

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,7 +1,21 @@
 print("           Welcome to the expense Tracker!")
 print("-----------------------------------------------------")
+balance = 0
+
+
+# Add income to the balance
+def income(balance):
+    print("              Add income to your balance")
+    print("-----------------------------------------------------")
+    print("")
+    print("How much money do you want to add?")
+    gained_money = int(input("> "))
+    balance += gained_money
+    return balance
+
 
 while True:
+    print(f"Your current balance is {balance}")
     print("What do you want to do?")
     print("1. Track your income")
     print("2. Track your expense")
@@ -16,7 +30,7 @@ while True:
         print("Please input a number between 1 and 4")
 
     if option == 1:
-        pass
+        balance = income(balance)
     elif option == 2:
         pass
     elif option == 3:
